3ef80481cc93b8761f3c90262fb5ffd64a8ae7fe
Magda Fairfax <magda.fairfax@gmail.com>
2019-11-01 18:43:06 +0000

    Fix main class for war deployment

--- src/main/java/com/vat/VatApplication.java
+++ src/main/java/com/vat/VatApplication.java
@@ -2,13 +2,20 @@ package com.vat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class VatApplication {
+public class VatApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VatApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VatApplication.class);
     }
 
 }
